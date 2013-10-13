91ff9db3eaae0057eddf4cf25c3f267d11655d86
juanmb <juanmb@pulseingenieria.com>
2013-10-13 18:06:09 +0200

    Rename the package (from opendaq to easydaq)

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-opendaq
+EasyDAQ
 =======
-Python binding for openDAQ hardware.
 
-openDAQ libraries and examples are compatible with Python 2.X.
-Python 3.X is not yet supported.
+GUI interface for openDAQ implemented in Python.
+
+EasyDAQ requires Python 2.5 or higher. Python 3 is currently not supported.
 
 * * *
 
-OpenDAQ is an open source data acquisition instrument, which provides user with
+OpenDAQ is an open source data acquisition instrument, which provides user
 several physical interaction capabilities such as analog inputs and outputs,
 digital inputs and outputs, timers and counters.
 
-By means of an USB connection, openDAQ brings all the information that it
-captures to a host computer, where you can decide how to process, display and
-store it. Several demos and examples are provided in website's support page.
+Through a USB connection, openDAQ brings all the information that it captures
+to a host computer, where you can decide how to process, display and store it.
+Several demos and examples are provided in website's support page.
 (http://www.open-daq.com/paginas/support)
 
 Please, go to http://www.open-daq.com for additional info.
--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from opendaq import __version__
+from easydaq import __version__
 
-DESCRIPTION = 'Python binding for openDAQ hardware'
-LONG_DESCRIPTION = """
-`OpenDAQ <http://www.open-daq.com/>`_ is an open source acquisition instrument \
-which provides several physical interaction capabilities such as analog \
-inputs and outputs, digital inputs and outputs, timers and counters.
-"""
+DESCRIPTION = "GUI interface for openDAQ"
+LONG_DESCRIPTION = """`OpenDAQ <http://www.open-daq.com/>`_ is an open source
+acquisition instrument which provides several physical interaction capabilities
+such as analog inputs and outputs, digital inputs and outputs, timers and
+counters."""
 
 
 setup(
-    name='opendaq',
+    name='easydaq',
     version=__version__,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    author='Juan Menendez',
-    author_email='juanmb@ingen10.com',
-    url='http://github.com/juanmb/python-opendaq',
+    author='Adrian Alvarez',
+    author_email='alvarez.lopez.adri@ingen10.com',
+    url='http://github.com/Ingen10/easydaq',
     license='LGPL',
     platforms=['any'],
-    install_requires=['pyserial'],
-    packages=['opendaq'],
+    install_requires=['pyserial', 'wx'],
+    packages=['easydaq'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
